5765e162580ccfcd3ad3dad757fe72f0cac38413
Denis Wolf <dear.mr.dw@gmail.com>
2015-04-01 14:48:59 +0200

    add chai.js and setup the test env

---
+++ tests.js
@@ -0,0 +1,9 @@
+var expect = chai.expect;
+
+describe("Sizify", function() {
+	describe("plugin", function() {
+		it("is defined", function() {
+			expect(typeof Sizify).to.equal("function");
+		});
+	});
+});
