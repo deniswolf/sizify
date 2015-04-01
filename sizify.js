10e39cd3d4ad6cc0b21c78a30b3836c2e0134ede
Denis Wolf <dear.mr.dw@gmail.com>
2015-04-01 15:15:09 +0200

    add helper for the form setup

--- sizify.js
+++ sizify.js
@@ -16,23 +16,24 @@
 		};
 
 		window.Sizify = function(form) {
+			var _sizify = {};
 
-			var fontFamily = $(form).find('[data-sizify=font-family]'),
-				fontSize = $(form).find('[data-sizify=font-size]'),
-				maxWidth = $(form).find('[data-sizify=max-width]'),
-				textInput = $(form).find('[data-sizify=text-input]'),
-				textWrapper = $(form).find('[data-sizify=text-wrapper]'),
-				textContainer = $(form).find('[data-sizify=text-container]'),
-				changeEvent = 'input propertychange';
+			var fontFamily = _sizify.fontFamily = $(form).find('[data-sizify=font-family]'),
+				fontSize = _sizify.fontSize = $(form).find('[data-sizify=font-size]'),
+				maxWidth = _sizify.maxWidth = $(form).find('[data-sizify=max-width]'),
+				textInput = _sizify.textInput = $(form).find('[data-sizify=text-input]'),
+				textWrapper = _sizify.textWrapper = $(form).find('[data-sizify=text-wrapper]'),
+				textContainer = _sizify.textContainer = $(form).find('[data-sizify=text-container]'),
+				changeEvent = _sizify.changeEvent = 'input propertychange';
 
 			fontFamily.on(changeEvent, updater.bind(fontFamily, textWrapper, 'font-family'));
 			fontSize.on(changeEvent, updater.bind(fontSize, textWrapper, 'font-size'));
 			maxWidth.on(changeEvent, updater.bind(maxWidth, textWrapper, 'max-width'));
 			textInput.on(changeEvent, updater.bind(textInput, textWrapper, 'text-input'));
 
+			return _sizify;
 		};
 
-		window.Sizify($('form'));
 	});
 
 })($);
--- tests.js
+++ tests.js
@@ -1,9 +1,34 @@
-var expect = chai.expect;
+	var expect = chai.expect;
 
-describe("Sizify", function() {
-	describe("plugin", function() {
-		it("is defined", function() {
-			expect(typeof Sizify).to.equal("function");
+	describe("Sizify", function() {
+		before(function() {
+			this.form = $('form');
+			var _sizify = this._sizify = Sizify(this.form);
+			this.setAllParams = function(font, size, width, text) {
+				_sizify.fontFamily.val(font);
+				_sizify.fontFamily.trigger(_sizify.changeEvent);
+				_sizify.fontSize.val(size);
+				_sizify.fontFamily.trigger(_sizify.changeEvent);
+				_sizify.maxWidth.val(width);
+				_sizify.maxWidth.trigger(_sizify.changeEvent);
+				_sizify.textInput.val(text);
+				_sizify.maxWidth.trigger(_sizify.changeEvent);
+			};
+
 		});
+		describe("plugin", function() {
+			it("is defined", function() {
+				expect(typeof Sizify).to.equal("function");
+			});
+		});
+
+		describe("helpers", function() {
+			describe('setAllParams', function() {
+				it('does not throw', function() {
+					this.setAllParams('serif', 10, 150, '');
+				});
+			});
+		});
+
+
 	});
-});
